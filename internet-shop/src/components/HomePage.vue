2dbd07b48b7c2b13cb4c83aaaed1b447eea79ea3
LimitEditions <anastasiya_sergeevna_khramova@mail.ru>
2024-02-21 20:23:12 +0300

    Домашнее задание 8(добавила переход на домашней странице

--- internet-shop/src/components/HomePage.vue
+++ internet-shop/src/components/HomePage.vue
@@ -4,17 +4,14 @@
         <main>
             <div class="container main">
                 <div class="main__desc">
-                    <h1 class="main__desc_headding">Let Your Home Be Unique
-                    </h1>
-                    <p class="main__desc_text">There are many variations of the passages of lorem Ipsum fromavailable,
-                        majority.</p>
+                    <h1 class="main__desc_headding">Let Your Home Be Unique</h1>
+                    <p class="main__desc_text">There are many variations of the passages of lorem Ipsum fromavailable, majority.</p>
                     <button class="main__desc_btn">
                         <p class="main__desc_btn-text">Get Started</p>
                         <svg class="main__desc_btn-img" xmlns="http://www.w3.org/2000/svg" width="18" height="17"
-                            viewBox="0 0 18 17" fill="none">
-                            <path d="M1 8.43542L14.7232 8.29857M9.61818
-                                1.91138L16.1412 8.43436L9.48677 15.0887" stroke="#CDA274" stroke-width="2"
-                                stroke-linecap="square" stroke-linejoin="round" />
+                            viewBox="0 0 18 17" fill="none"><path d="M1 8.43542L14.7232 8.29857M9.61818
+                            1.91138L16.1412 8.43436L9.48677 15.0887" stroke="#CDA274" stroke-width="2"
+                            stroke-linecap="square" stroke-linejoin="round" />
                         </svg>
                     </button>
                 </div>
@@ -23,10 +20,7 @@
         <section>
             <div class="container subheadding">
                 <h2 class="subheadding__title">Follow Our Projects</h2>
-                <p class="subheadding__text">It is a long established fact
-                    that a reader will be distracted by the of readable
-                    content of page lookings at its layouts points.
-                </p>
+                <p class="subheadding__text">It is a long established fact that a reader will be distracted by the of readable content of page lookings at its layouts points. </p>
             </div>
         </section>
         <section>
@@ -36,49 +30,35 @@
                     <div class="cards__item_desc">
                         <div>
                             <p class="cards__item_text">Modern Kitchan</p>
-                            <p class="cards__item_sort">Decor /
-                                Artchitecture</p>
+                            <p class="cards__item_sort">Decor /Artchitecture</p>
                         </div>
-                        <button class="cards__item_btn"><svg xmlns="http://www.w3.org/2000/svg" width="70" height="70"
-                                viewBox="0 0 70 70" fill="none">
-                                <circle cx="35" cy="35" r="35" fill="#F4F0EC" />
-                                <path d="M32 44L40 35L32 26" stroke="#292F36" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                            </svg>
-                        </button>
+                        <router-link :to="url">
+                            <button class="cards__item_btn"><img class="cards__item_btn-vector" src="@/assets/img/Vector.png" alt="vector"> </button>
+                        </router-link>
                     </div>
                 </div>
                 <div class="cards__item">
                     <img class="cards__item_img_top-left" src="@/assets/img/Card_2.png" alt="Card_2">
                     <div class="cards__item_desc">
                         <div>
-                            <p class="cards__item_text">Modern
-                                Kitchan</p>
-                            <p class="cards__item_sort">Decor /
-                                Artchitecture</p>
+                            <p class="cards__item_text">Modern Kitchan</p>
+                            <p class="cards__item_sort">Decor / Artchitecture</p>
                         </div>
-                        <button class="cards__item_btn"><svg xmlns="http://www.w3.org/2000/svg" width="70" height="70"
-                                viewBox="0 0
-                                            70 70" fill="none">
-                                <circle cx="35" cy="35" r="35" fill="#F4F0EC" />
-                                <path d="M32 44L40 35L32 26" stroke="#292F36" stroke-width="2" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                            </svg>
-                        </button>
+                        <router-link :to="url">
+                            <button class="cards__item_btn"><img class="cards__item_btn-vector" src="@/assets/img/Vector.png" alt="vector"> </button>
+                        </router-link>
                     </div>
                 </div>
                 <div class="cards__item">
                     <img class="cards__item_img_bottom-right" src="@/assets/img/Card_3.png" alt="Card_3">
                     <div class="cards__item_desc">
                         <div>
-                            <p class="cards__item_text">Modern
-                                Kitchan</p>
-                            <p class="cards__item_sort">Decor
-                                / Artchitecture</p>
+                            <p class="cards__item_text">Modern Kitchan</p>
+                            <p class="cards__item_sort">Decor / Artchitecture</p>
                         </div>
-                        <button class="cards__item_btn"><img class="cards__item_btn-vector" src="@/assets/img/Vector.png"
-                                alt="vector">
-                        </button>
+                        <router-link :to="url">
+                            <button class="cards__item_btn"><img class="cards__item_btn-vector" src="@/assets/img/Vector.png" alt="vector"> </button>
+                        </router-link>
                     </div>
                 </div>
                 <div class="cards__item">
@@ -90,15 +70,9 @@
                             <p class="cards__item_sort">Decor
                                 / Artchitecture</p>
                         </div>
-                        <button class="cards__item_btn"><svg xmlns="http://www.w3.org/2000/svg" width="70" height="70"
-                                viewBox="0 0 70 70" fill="none">
-                                <circle cx="35" cy="35" r="35" fill="#F4F0EC" />
-                                <path d="M32
-                                                                    44L40 35L32
-                                                                    26" stroke="#292F36" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>
-                        </button>
+                        <router-link :to="url">
+                            <button class="cards__item_btn"><img class="cards__item_btn-vector" src="@/assets/img/Vector.png" alt="vector"> </button>
+                        </router-link>
                     </div>
                 </div>
             </div>
@@ -108,23 +82,19 @@
                 <div class="count__counter">
                     <div class="count__counter_block">
                         <h3 class="count__num">12</h3>
-                        <p class="count__text">Years
-                            Of Experiance</p>
+                        <p class="count__text">Years Of Experiance</p>
                     </div>
                     <div class="count__counter_block">
                         <h3 class="count__num">85</h3>
-                        <p class="count__text">
-                            Success Project</p>
+                        <p class="count__text">Success Project</p>
                     </div>
                     <div class="count__counter_block">
                         <h3 class="count__num">15</h3>
-                        <p class="count__text">Active
-                            Project</p>
+                        <p class="count__text">Active Project</p>
                     </div>
                     <div class="count__counter_block">
                         <h3 class="count__num">95</h3>
-                        <p class="count__text">Happy
-                            CUstomers</p>
+                        <p class="count__text">Happy Customers</p>
                     </div>
                 </div>
             </div>
@@ -140,7 +110,12 @@ import MainCards from './MainCards.vue';
 import Start from './Start.vue';
 
 export default {
-    components: { Start, End, MainCards }
+    components: { Start, End, MainCards },
+    data() {
+        return {
+            url: '/project',
+        }
+    },
 }
 </script>
 
